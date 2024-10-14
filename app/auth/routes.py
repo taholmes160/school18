371dcd454d17103a29e7197e772bbc9257417655
Tim Holmes <taholmes160@gmail.com>
2024-10-14 04:33:27 -0400

    Refactor login page and add form validation

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, login_required, logout_user
+from app import db
+from app.models import User
 from app.auth import auth_bp
+from app.forms import LoginForm
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('profile'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
